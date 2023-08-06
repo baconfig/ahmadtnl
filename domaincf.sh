4e4521a4f5c02505ca2830be0c14b6eb123417b4
baconfig <133508186+baconfig@users.noreply.github.com>
2023-08-06 20:02:38 +0700

    Update domaincf.sh

--- domaincf.sh
+++ domaincf.sh
@@ -90,10 +90,10 @@ cd
 function randomsubdomain() {
 #SUB=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 MYIP=$(wget -qO- icanhazip.com);
-CF_ID=yadicakepp@gmail.com
-CF_KEY=b22d286c2d7f6d3e5073325dd18b76ca4ddb2
+CF_ID=centerdata96@gmail.com
+CF_KEY=0adfcabdd8e54a57dc5af4fb08dd1041cbea7
 SUB=$(</dev/urandom tr -dc a-z0-9 | head -c3)
-DOMAIN=ahmadstore.pw
+DOMAIN=config.biz.id
 SUB_DOMAIN=tensai.${SUB}.${DOMAIN}
 NS_DOMAIN=ns.${SUB_DOMAIN}
 echo "DOMAIN kamu adalah : ${SUB_DOMAIN}"
